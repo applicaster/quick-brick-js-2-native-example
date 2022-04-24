9157ceaf4451611a433763b8aedbd5cfcfb2c953
Francois roland <f.roland@applicaster.com>
2022-04-24 17:44:32 +0200

    fix: add safeAreaInsets

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,20 @@
-import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
-import {
-  DefaultTheme,
-  Provider as PaperProvider,
-  BottomNavigation,
-} from "react-native-paper";
+import React from "react";
+import { StyleSheet } from "react-native";
 
 import dynamic from "next/dynamic";
-import Headers from "../src/Headers";
 
-const HooksExample = dynamic(() => import("../src/HooksExample"), {
-  ssr: false,
-});
-const StorageExample = dynamic(() => import("../src/StorageExample"), {
-  ssr: false,
+const Router = dynamic(() => import("../src/Router"), {
+  ssr: false
 });
 
 export default function Home(props) {
-  const [index, setIndex] = useState(0);
-
-  const [routes] = React.useState([
-    { key: "headers", title: "Headers", icon: "web", props },
-    { key: "hooks", title: "Hooks", icon: "webhook" },
-    { key: "storage", title: "Storage", icon: "database" },
-  ]);
-
-  const renderScene = BottomNavigation.SceneMap({
-    headers: Headers,
-    hooks: HooksExample,
-    storage: StorageExample,
-  });
-
-  return (
-    <View style={styles.container}>
-      <PaperProvider theme={DefaultTheme}>
-        <BottomNavigation
-          renderScene={renderScene}
-          onIndexChange={setIndex}
-          navigationState={{ index, routes }}
-        />
-      </PaperProvider>
-    </View>
-  );
+  return <Router {...props} />;
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-  },
+    flex: 1
+  }
 });
 
 Home.getInitialProps = async (ctx) => {
---
+++ src/Router.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import {
+  DefaultTheme,
+  Provider as PaperProvider,
+  BottomNavigation
+} from "react-native-paper";
+
+import Headers from "./Headers";
+import HooksExample from "./HooksExample";
+import StorageExample from "./StorageExample";
+
+import JS2Native from "@applicaster/quick-brick-js-2-native";
+
+type Props = {
+  foo: string;
+  header: unknown;
+};
+
+export default function Router(props: Props) {
+  const { safeAreaInsets } = JS2Native();
+  console.log({ safeAreaInsets });
+
+  const [index, setIndex] = useState(0);
+
+  const [routes] = React.useState([
+    { key: "headers", title: "Headers", icon: "web", props },
+    { key: "hooks", title: "Hooks", icon: "webhook" },
+    { key: "storage", title: "Storage", icon: "database" }
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    headers: Headers,
+    hooks: HooksExample,
+    storage: StorageExample
+  });
+
+  return (
+    <View style={[styles.container, { paddingTop: safeAreaInsets?.top || 0 }]}>
+      <PaperProvider theme={DefaultTheme}>
+        <BottomNavigation
+          renderScene={renderScene}
+          onIndexChange={setIndex}
+          navigationState={{ index, routes }}
+          safeAreaInsets={safeAreaInsets}
+        />
+      </PaperProvider>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
